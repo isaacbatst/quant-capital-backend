67edc799259bbb7015f74d8065624706d0513a0c
Isaac Batista <isaacbatst@gmail.com>
2022-12-31 11:11:04 -0300

    style: fix email address lint

--- src/domain/entities/EmailAddress.test.ts
+++ src/domain/entities/EmailAddress.test.ts
@@ -1,15 +1,15 @@
-import { EmailAddress } from "./EmailAddress"
+import {EmailAddress} from './EmailAddress';
 
 describe('Email', () => {
-  it('should create valid email address', () => {
-    const email = new EmailAddress('valid@email.com')
+	it('should create valid email address', () => {
+		const email = new EmailAddress('valid@email.com');
 
-    expect(email.value).toBe('valid@email.com')
-  })
+		expect(email.value).toBe('valid@email.com');
+	});
 
-  it('should not create an invalid email address', () => {
-    expect(() => {
-      const email = new EmailAddress('invalidemail.com')
-    }).toThrow('INVALID_EMAIL')
-  })
-})
+	it('should not create an invalid email address', () => {
+		expect(() => {
+			const email = new EmailAddress('invalidemail.com');
+		}).toThrow('INVALID_EMAIL');
+	});
+});
--- src/domain/entities/EmailAddress.ts
+++ src/domain/entities/EmailAddress.ts
@@ -1,13 +1,15 @@
 export class EmailAddress {
-  static readonly regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+	static get regex() {
+		return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+	}
 
-  constructor(readonly value: string){
-    this.validateEmail()
-  }
+	constructor(readonly value: string) {
+		this.validateEmail();
+	}
 
-  private validateEmail(){
-    if(!EmailAddress.regex.test(this.value)) {
-      throw new Error('INVALID_EMAIL')
-    }
-  }
+	private validateEmail() {
+		if (!EmailAddress.regex.test(this.value)) {
+			throw new Error('INVALID_EMAIL');
+		}
+	}
 }
